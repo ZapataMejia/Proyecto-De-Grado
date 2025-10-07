75b914ac65c0977b8581b61829743e8fd5d3c1d1
Santiago Zapata <Szapatamejia193@gmail.com>
2025-10-06 19:54:58 -0500

    Update button colors from 'dark' to 'black' for consistency across components

--- src/components/about/footer.tsx
+++ src/components/about/footer.tsx
@@ -39,7 +39,7 @@ export function FooterOne() {
               </Typography>
               <div className="flex flex-col gap-3 sm:flex-row">
                 <Input color="black" label="Enter your email" />
-                <Button color="dark">subscribe</Button>
+                <Button color="black">subscribe</Button>
               </div>
             </div>
           </div>
--- src/components/defaultNavbar.tsx
+++ src/components/defaultNavbar.tsx
@@ -228,7 +228,7 @@ export default function Example() {
               </Button>
             </a>
             <a href="/astro-launch-ui/signup">
-              <Button size="sm" color="dark">
+              <Button size="sm" color="black">
                 Sign Up
               </Button>
             </a>
@@ -250,12 +250,12 @@ export default function Example() {
           <NavList />
           <div className="flex w-full flex-nowrap items-center gap-2 lg:hidden">
             <a href="/astro-launch-ui/login">
-              <Button variant="outlined" size="sm" color="dark" fullWidth>
+              <Button variant="outlined" size="sm" color="black" fullWidth>
                 Sign In
               </Button>
             </a>
             <a href="/astro-launch-ui/signup">
-              <Button size="sm" fullWidth color="dark">
+              <Button size="sm" fullWidth color="black">
                 Sign Up
               </Button>
             </a>
